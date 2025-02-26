e03c860bdc01724b3a386066afe6084bf6ac3ea3
Lucas Dantas <lucasdantasprogramador@gmail.com>
2025-02-26 08:29:34 -0300

    remove login url redirect function

--- rd-exclusive-plugin.php
+++ rd-exclusive-plugin.php
@@ -13,4 +13,4 @@ if (!function_exists('add_action')) die;
 
 require_once plugin_dir_path(__FILE__) . 'src/admin/settings.php';
 require_once plugin_dir_path(__FILE__) . 'src/frontend/custom-login-header.php';
-require_once plugin_dir_path(__FILE__) . 'src/rewrite/rules.php';
+//require_once plugin_dir_path(__FILE__) . 'src/rewrite/rules.php';
