373c6bcd36e93c53cbc6881df539d4300a6bd731
Lucas Dantas <lucasdantasprogramador@gmail.com>
2025-06-09 18:26:11 -0300

    creating new script option

---
+++ src/modules/script-default-select-option-disabled/assets/js/script-default-select-option-disabled.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // Seleciona ambos os campos com o ID 'form-field-valor'
+  const allSelects = document.querySelectorAll('.exclusiveForm select');
+
+  // Itera sobre cada campo 'select' encontrado
+  allSelects.forEach(selectElement => {
+    // Cria a opção desabilitada
+    const defaultOption = document.createElement('option');
+    defaultOption.value = '';
+    defaultOption.textContent = ' ';
+    defaultOption.disabled = true;
+    defaultOption.selected = true;  // Define como opção selecionada padrão
+
+    // Adiciona a opção ao início do select
+    selectElement.prepend(defaultOption);
+  });
+});
+
